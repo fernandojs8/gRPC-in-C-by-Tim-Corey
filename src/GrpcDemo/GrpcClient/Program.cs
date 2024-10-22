db80d6a4ae3921b694f73d4ca83aded604954899
fernando.js <fernando.js8@outlook.com>
2024-10-22 20:11:12 -0300

    Done
    Done

---
+++ src/GrpcDemo/GrpcClient/Program.cs
@@ -0,0 +1,49 @@
+﻿using Grpc.Net.Client;
+using GrpcServer;
+using System;
+using System.Runtime.ConstrainedExecution;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace GrpcClient
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            //var helloRequest = new HelloRequest() { Name = "fer.js" };
+            //var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            //var client = new Greeter.GreeterClient(channel);
+
+            //var reply = await client.SayHelloAsync(helloRequest);
+
+            //Console.WriteLine(reply.Message);
+
+            var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var customerService = new Customer.CustomerClient(channel);
+
+            var customerRequest = new CustomerLookUpModel() { UserId = 2 };
+
+            var reply = await customerService.GetCustomerInfoAsync(customerRequest);
+
+            Console.WriteLine($"Firstname: {reply.FirstName}, LastName: {reply.LastName}");
+            var cancellationTokenSource = new CancellationTokenSource();
+
+            Console.WriteLine("New Customer List");
+
+            using (var call = customerService.GetNewCustomers(new NewCustomerModel()))
+            {
+                while (await call.ResponseStream.MoveNext(cancellationTokenSource.Token))
+                {
+                    var currentCustomer = call.ResponseStream.Current;
+                    Console.WriteLine($"Name: {currentCustomer.FirstName} {currentCustomer.LastName}, " +
+                                          $"Email: {currentCustomer.EmailAddress}, " +
+                                          $"Age: {currentCustomer.Age}");
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ src/GrpcDemo/GrpcServer/Services/CustomersService.cs
@@ -0,0 +1,75 @@
+﻿using Grpc.Core;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace GrpcServer.Services
+{
+    public class CustomersService : Customer.CustomerBase
+    {
+        private readonly ILogger _logger;
+
+        public CustomersService(ILogger<CustomersService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override Task<CustomerModel> GetCustomerInfo(CustomerLookUpModel request, ServerCallContext context)
+        {
+            CustomerModel customerModel = new CustomerModel();
+
+            if (request.UserId == 1)
+            {
+                customerModel.FirstName = "Jamie";
+                customerModel.LastName = "Smith";
+            }
+            else if(request.UserId == 2)
+            {
+                customerModel.FirstName = "Jane";
+                customerModel.LastName = "Doe";
+            }else
+            {
+                customerModel.FirstName = "Greg";
+                customerModel.LastName = "Thomas";
+            }
+
+            return Task.FromResult(customerModel);
+        }
+
+        public override async Task GetNewCustomers(NewCustomerModel request, IServerStreamWriter<CustomerModel> responseStream, ServerCallContext context)
+        {
+            List<CustomerModel> customers = new List<CustomerModel>
+            {
+                new CustomerModel
+                {
+                    FirstName = "Fer",
+                    LastName = "JS",
+                    Age = 35,
+                    EmailAddress = "fernando.js8@outlook.com",
+                    IsAlive = true,
+                },
+                new CustomerModel
+                {
+                    FirstName = "Sue",
+                    LastName = "Storm",
+                    Age = 28,
+                    EmailAddress = "sue@stormy.net",
+                    IsAlive = false,
+                },
+                new CustomerModel
+                {
+                    FirstName = "Bilbo",
+                    LastName = "Baggins",
+                    Age = 117,
+                    EmailAddress = "bilbo@midleearth.net",
+                    IsAlive = false,
+                },
+            };
+
+            foreach (var customer in customers)
+            {
+                await responseStream.WriteAsync(customer);
+            }
+        }
+    }
+}
